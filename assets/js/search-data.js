7b7d0856722badb87a4ff076e716e8306db75263
miladpour <miladh@umich.edu>
2025-11-24 17:00:29 +0000

    Deploying to gh-pages from @ miladpour/miladpour.github.io@c939604d13afc3cce271f76fe321f5f17ad597ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,7 +497,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%69%6C%61%64%68@%75%6D%69%63%68.%65%64%75", "_blank");
         },
       },{
         id: 'social-linkedin',
