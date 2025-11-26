f48cb3847bb7101c2e13d6f96267143da427d69f
miladpour <miladh@umich.edu>
2025-11-26 02:30:04 +0000

    Deploying to gh-pages from @ miladpour/miladpour.github.io@c34fa828b64ae363d99e83dd8a557e58b776d24c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-our-paper-domain-constrained-diffusion-models-to-synthesize-tabular-data-a-case-study-in-power-systems-is-accepted-for-presentation-at-the-2025-icml-workshop-dataworld-unifying-data-curation-frameworks-across-domains-poster",
+            },},{id: "news-i-was-honored-to-receive-the-student-poster-competition-award-at-the-institute-for-energy-solutions-2025-energy-symposium",
+          title: 'I was honored to receive the Student Poster Competition Award at the Institute...',
+          description: "",
+          section: "News",},{id: "news-our-paper-domain-constrained-diffusion-models-to-synthesize-tabular-data-a-case-study-in-power-systems-is-accepted-for-presentation-at-the-2025-icml-workshop-dataworld-unifying-data-curation-frameworks-across-domains-poster",
           title: 'Our paper Domain-constrained diffusion models to synthesize tabular data: A case study in...',
           description: "",
           section: "News",},{id: "news-our-new-paper-constrained-diffusion-models-for-synthesizing-representative-power-flow-datasets-has-been-released-on-arxiv",
