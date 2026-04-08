49d2fe7d75b5c5177378d11af24f35c607b3ae7e
miladpour <miladh@umich.edu>
2026-04-08 21:02:52 +0000

    Deploying to gh-pages from @ miladpour/miladpour.github.io@75286ccf720f3b83337e91ea55f6ca1704b075c5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -21,7 +21,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-was-honored-to-receive-the-student-poster-competition-award-at-the-institute-for-energy-solutions-2025-energy-symposium",
+            },},{id: "news-our-paper-constrained-diffusion-models-for-synthesizing-representative-power-flow-datasets-has-been-accepted-for-publication-in-ieee-transactions-on-power-systems",
+          title: 'Our paper “Constrained Diffusion Models for Synthesizing Representative Power Flow Datasets” has been...',
+          description: "",
+          section: "News",},{id: "news-i-was-honored-to-receive-the-student-poster-competition-award-at-the-institute-for-energy-solutions-2025-energy-symposium",
           title: 'I was honored to receive the Student Poster Competition Award at the Institute...',
           description: "",
           section: "News",},{id: "news-our-paper-domain-constrained-diffusion-models-to-synthesize-tabular-data-a-case-study-in-power-systems-is-accepted-for-presentation-at-the-2025-icml-workshop-dataworld-unifying-data-curation-frameworks-across-domains-poster",
